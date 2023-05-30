bb2a0ddc18ca55958cfa5efd8cd3f32ee42ed8bd
NuggetNut <129174929+JimmyUns@users.noreply.github.com>
2023-05-30 16:56:24 +0300

    Add project files.

---
+++ My Face Tracker/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace My_Face_Tracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCamOutput = new System.Windows.Forms.PictureBox();
+            this.btnCamStart = new System.Windows.Forms.Button();
+            this.btnCamStop = new System.Windows.Forms.Button();
+            this.btnFaceTracking = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picCamOutput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picCamOutput
+            // 
+            this.picCamOutput.Location = new System.Drawing.Point(12, 90);
+            this.picCamOutput.Name = "picCamOutput";
+            this.picCamOutput.Size = new System.Drawing.Size(518, 348);
+            this.picCamOutput.TabIndex = 0;
+            this.picCamOutput.TabStop = false;
+            this.picCamOutput.Paint += new System.Windows.Forms.PaintEventHandler(this.picCamOutput_Paint);
+            // 
+            // btnCamStart
+            // 
+            this.btnCamStart.Location = new System.Drawing.Point(12, 12);
+            this.btnCamStart.Name = "btnCamStart";
+            this.btnCamStart.Size = new System.Drawing.Size(118, 28);
+            this.btnCamStart.TabIndex = 1;
+            this.btnCamStart.Text = "Start Camera";
+            this.btnCamStart.UseVisualStyleBackColor = true;
+            this.btnCamStart.Click += new System.EventHandler(this.btnCamStart_Click);
+            // 
+            // btnCamStop
+            // 
+            this.btnCamStop.Location = new System.Drawing.Point(12, 56);
+            this.btnCamStop.Name = "btnCamStop";
+            this.btnCamStop.Size = new System.Drawing.Size(118, 28);
+            this.btnCamStop.TabIndex = 2;
+            this.btnCamStop.Text = "Stop Camera";
+            this.btnCamStop.UseVisualStyleBackColor = true;
+            this.btnCamStop.Click += new System.EventHandler(this.btnCamStop_Click);
+            // 
+            // btnFaceTracking
+            // 
+            this.btnFaceTracking.Location = new System.Drawing.Point(155, 12);
+            this.btnFaceTracking.Name = "btnFaceTracking";
+            this.btnFaceTracking.Size = new System.Drawing.Size(119, 72);
+            this.btnFaceTracking.TabIndex = 4;
+            this.btnFaceTracking.Text = "Start Face Tracking";
+            this.btnFaceTracking.UseVisualStyleBackColor = true;
+            this.btnFaceTracking.Click += new System.EventHandler(this.btnFaceTracking_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(297, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 53);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(543, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnFaceTracking);
+            this.Controls.Add(this.btnCamStop);
+            this.Controls.Add(this.btnCamStart);
+            this.Controls.Add(this.picCamOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picCamOutput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCamOutput;
+        private System.Windows.Forms.Button btnCamStart;
+        private System.Windows.Forms.Button btnCamStop;
+        private System.Windows.Forms.Button btnFaceTracking;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ My Face Tracker/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Face;
+using Emgu.CV.Structure;
+
+
+namespace My_Face_Tracker
+{
+    public partial class Form1 : Form
+    {
+        bool facetrackingEnabled = false;
+
+        Capture vidCapture = null;
+        Mat frame = null;
+        private Image<Bgr, Byte> currentFrame = null;
+
+        CascadeClassifier faceCasacdeClassifier = new CascadeClassifier("haarcascade_frontalface_alt.xml");
+
+        PersonInCamera[] personInCamera = new PersonInCamera[0];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCamStart_Click(object sender, EventArgs e)
+        {
+            vidCapture = new Capture();
+            vidCapture.Start();
+            Application.Idle += CameraManager;
+        }
+
+        private void CameraManager(object sender, EventArgs e)
+        {
+            try
+            {
+                //Display Camera
+                frame = vidCapture.QueryFrame();
+                currentFrame = frame.ToImage<Bgr, Byte>().Resize(picCamOutput.Width, picCamOutput.Height, Inter.Cubic);
+
+                //Mirror on y axis currentFrame
+                Bitmap currentFrameMirrored = currentFrame.ToBitmap();
+                currentFrameMirrored.RotateFlip(RotateFlipType.Rotate180FlipY);
+                currentFrame = new Image<Bgr, byte>(currentFrameMirrored);
+
+                picCamOutput.Image = currentFrame.Bitmap;
+
+                //Face Detection
+                if (facetrackingEnabled)
+                {
+                    Mat grayMat = new Mat();
+                    CvInvoke.CvtColor(currentFrame, grayMat, ColorConversion.Bgr2Gray);
+                    CvInvoke.EqualizeHist(grayMat, grayMat);
+
+                    Rectangle[] faces = faceCasacdeClassifier.DetectMultiScale(grayMat, 1.1, 3, Size.Empty, Size.Empty);
+
+                    if (faces.Length > 0)
+                    {
+                        personInCamera = new PersonInCamera[faces.Length];
+                        for (int i = 0; i < personInCamera.Length; i++)
+                        {
+                            personInCamera[i] = new PersonInCamera();
+                            personInCamera[i].name = "None";
+                            personInCamera[i].posVector2[0] = faces[i].Location.X;
+                            personInCamera[i].posVector2[1] = faces[i].Location.Y;
+                            personInCamera[i].sizeVector2[0] = faces[i].Size.Width;
+                            personInCamera[i].sizeVector2[1] = faces[i].Size.Height;
+                        }
+                        Image<Bgr, Byte> resultImage = currentFrame.Convert<Bgr, Byte>();
+                        resultImage.ROI = faces[0];
+                        pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                        pictureBox1.Image = resultImage.Bitmap;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                StopCamera();
+            }
+
+        }
+
+        private void btnCamStop_Click(object sender, EventArgs e)
+        {
+            StopCamera();
+        }
+
+        private void StopCamera()
+        {
+            Application.Idle -= CameraManager;
+            vidCapture.Dispose();
+            picCamOutput.Image = null;
+
+            if(facetrackingEnabled) btnFaceTracking_Click(null, null);
+        }
+
+        private void btnFaceTracking_Click(object sender, EventArgs e)
+        {
+            if(facetrackingEnabled)
+            {
+                btnFaceTracking.Text = "Start Face Tracking";
+            } else
+            {
+                btnFaceTracking.Text = "Stop Face Tracking";
+            }
+            facetrackingEnabled = !facetrackingEnabled;
+        }
+
+        private void picCamOutput_Paint(object sender, PaintEventArgs e)
+        {
+            if (!facetrackingEnabled) return;
+            if(personInCamera.Length > 0)
+            {
+                for (int i = 0; i < personInCamera.Length; i++)
+                {
+                    int[] posVector2 = personInCamera[i].posVector2;
+                    int[] sizeVector2 = personInCamera[i].sizeVector2;
+
+                    Rectangle rectArea = new Rectangle(posVector2[0], posVector2[1], sizeVector2[0], sizeVector2[1]);
+
+
+                    using (Pen pen = new Pen(Color.Red, 2))
+                    {
+                        e.Graphics.DrawRectangle(pen, rectArea);
+                    }
+
+                    using (Font myFont = new Font("Arial", 14))
+                    {
+                        e.Graphics.DrawString(personInCamera[i].name, myFont, Brushes.Green, new Point(posVector2[0], posVector2[1]));
+                    }
+                }
+            }
+
+        }
+    }
+
+    class PersonInCamera
+    {
+        public string name = "None";
+        public int[] posVector2 = new int[2];
+        public int[] sizeVector2 = new int[2];
+    }
+}
